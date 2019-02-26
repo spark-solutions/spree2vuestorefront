5b9125d7fbcc656ed616794397f01f741bee52e0
tniezg <tomek.niezgoda@mail.com>
2019-02-26 12:45:54 +0100

    Add basic cart implementation

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,198 @@
 import cors from 'cors'
 import express from 'express'
 import Instance from 'spree-storefront-api-v2-js-sdk/src/Instance'
+import { JsonApiSingleResponse } from '../interfaces'
 import {
+  findIncluded,
+  findIncludedOfType,
+  getLineItem,
+  getTokenOptions,
   logger,
+  variantFromSku
 } from '../utils'
 
 const app = express()
 
-export default (_: Instance) => {
+export default (spreeClient: Instance) => {
   app.use(cors())
   app.use(express.json())
+
+  app.post('/api/cart/create', (_, response) => {
+    logger.info('Fetching new cart token for guest user.')
+    spreeClient.cart.create()
+      .then((spreeResponse) => {
+        logger.info('New token for guest user fetched.')
+        const spreeToken = spreeResponse.data.attributes.token
+
+        response.json({
+          code: 200,
+          result: spreeToken
+        })
+      })
+      .catch((error) => {
+        logger.error(['Cannot get new cart token for guest user.', error])
+        response.statusCode = 500
+        response.json({
+          code: 500,
+          result: null
+        })
+      })
+  })
+
+  app.get('/api/cart/pull', (request, response) => {
+    logger.info('Fetching cart')
+    const cartId = request.query.cartId
+
+    const extraParams = {
+      include: [
+        'line_items',
+        'line_items.variant',
+        'line_items.variant.product',
+        'line_items.variant.product.option_types'
+      ].join(',')
+    }
+
+    spreeClient.cart.show(getTokenOptions(request), extraParams)
+      .then((spreeResponse: JsonApiSingleResponse) => {
+        logger.info('Cart fetched')
+        logger.info(spreeResponse)
+
+        const lineItems = findIncludedOfType(spreeResponse, spreeResponse.data, 'line_items')
+        const result = lineItems.map((lineItem) => {
+          return getLineItem(spreeResponse, lineItem, cartId)
+        })
+
+        response.json({
+          code: 200,
+          result
+        })
+      })
+      .catch((error) => {
+        logger.error(['Cannot get cart.', error])
+        response.statusCode = 500
+        response.json({
+          code: 500,
+          result: null
+        })
+      })
+  })
+
+  app.get('/api/cart/payment-methods', (request, response) => {
+    logger.info('Fetching payment methods')
+
+    spreeClient.checkout.paymentMethods(getTokenOptions(request))
+      .then((spreeResponse) => {
+        const paymentMethods = spreeResponse.data.map((paymentMethod) => {
+          return {
+            code: paymentMethod.id,
+            title: paymentMethod.attributes.name
+          }
+        })
+
+        response.json({
+          code: 200,
+          result: paymentMethods
+        })
+      })
+      .catch((error) => {
+        logger.error(['Cannot get payment methods.', error])
+        response.statusCode = 500
+        response.json({
+          code: 500,
+          result: null
+        })
+      })
+  })
+
+  app.post('/api/cart/update', (request, response) => {
+    const cartId = request.query.cartId
+    logger.info(`Updating cart for cartId = ${cartId}`)
+
+    const variantSku = request.body.cartItem.sku
+    const quantity = request.body.cartItem.qty
+
+    logger.info(`Finding variant with sku = ${variantSku}`)
+    variantFromSku(spreeClient, variantSku)
+      .then((spreeResponse: JsonApiSingleResponse) => {
+        logger.info(`Variant with sku = ${variantSku} found.`)
+        // TODO: do I have to react to configurable_item_options in cart/update payload??
+        const variant = spreeResponse.data
+        logger.info(`Adding qty = ${quantity} to variant.id = ${variant.id}`)
+        spreeClient.cart.addItem(
+          getTokenOptions(request),
+          {
+            include: [
+              'line_items',
+              'line_items.variant',
+              'line_items.variant.product',
+              'line_items.variant.product.option_types'
+            ].join(','),
+            quantity,
+            variant_id: +variant.id
+          }
+        )
+          .then((r: JsonApiSingleResponse) => {
+            logger.info(`Line item for variant sku = ${variantSku} added to cart.`)
+            const cart = r.data
+            const lineItems = findIncludedOfType(r, cart, 'line_items')
+            const addedLineItem = lineItems.find((lI) => {
+              const vr = findIncluded(r, lI.relationships.variant.data.type, lI.relationships.variant.data.id)
+              return vr.attributes.sku === variantSku
+            })
+            const convertedLineItem = getLineItem(r, addedLineItem, cartId)
+            response.json({
+              code: 200,
+              result: convertedLineItem
+            })
+          })
+          .catch((err) => {
+            console.error('ERRR', err)
+            response.statusCode = 500
+            response.json({
+              code: 500,
+              result: null
+            })
+          })
+      })
+
+    // TODO: support spreeClient.cart.removeItem
+    // TODO: support spreeClient.cart.setQuantity
+  })
+
+  app.get('/api/stock/check', (request, response) => {
+    const sku = request.query.sku
+    response.json({
+      code: 200,
+      result: {
+        backorders: 0,
+        enable_qty_increments: false,
+        is_decimal_divided: false,
+        is_in_stock: true,
+        is_qty_decimal: false,
+        item_id: 580,
+        low_stock_date: null,
+        manage_stock: true,
+        max_sale_qty: 10000,
+        min_qty: 0,
+        min_sale_qty: 1,
+        notify_stock_qty: 1,
+        product_id: 580,
+        qty: 53,
+        qty_increments: 0,
+        show_default_notification_message: false,
+        stock_id: 1,
+        stock_status_changed_auto: 0,
+        use_config_backorders: true,
+        use_config_enable_qty_inc: true,
+        use_config_manage_stock: true,
+        use_config_max_sale_qty: true,
+        use_config_min_qty: true,
+        use_config_min_sale_qty: 1,
+        use_config_notify_stock_qty: true,
+        use_config_qty_increments: true
+      }
+    })
+  })
 
   app.all('*', (request, response) => {
     logger.info(`Request for ${request.path} could not be handled`)
--- src/utils/product.ts
+++ src/utils/product.ts
@@ -1,4 +1,6 @@
 import Instance from 'spree-storefront-api-v2-js-sdk/src/Instance'
+import { findIncluded, findIncludedOfType, mapPages } from '.'
+import { ESProductType, JsonApiDocument, JsonApiResponse, JsonApiSingleResponse } from '../interfaces'
 
 // productCustomAttributesPrefix is used as extra prefix to reduce the possibility of naming collisions with product
 // options and standard product fields.
@@ -17,8 +19,84 @@ const findOptionTypeFromOptionValue = (optionTypes: any[], optionValueId): any |
   }) || null
 }
 
+const getLineItem = (response: JsonApiResponse, lineItem: JsonApiDocument, cartId: string) => {
+  // TODO: roundabout way of figuring out if product has variants. find better?
+  const variantIdentifier = lineItem.relationships.variant.data
+  const variant = findIncluded(response, variantIdentifier.type, variantIdentifier.id)
+  const productIdentifier = variant.relationships.product.data
+  const product = findIncluded(response, productIdentifier.type, productIdentifier.id)
+  const optionTypes = findIncludedOfType(response, product, 'option_types')
+  const productVariants = findIncludedOfType(response, product, 'variants')
+
+  const productOption: any = {}
+  if (productVariants.length > 0) {// TODO: also check if there are options
+    const configurableItemOptions = variant.relationships.option_values.data.map((ov) => {
+        const optionType = findOptionTypeFromOptionValue(optionTypes, ov.id)
+
+        return {
+          option_id: generateOptionAttributeCode(optionType.id),
+          option_value: ov.id
+        }
+    })
+    productOption.extension_attributes = { configurable_item_options: configurableItemOptions }
+  }
+
+  return {
+    item_id: lineItem.id,
+    name: lineItem.attributes.name,
+    price: lineItem.attributes.price,
+    product_type: productVariants.length === 0 ? ESProductType.Simple : ESProductType.Configurable,
+    qty: lineItem.attributes.quantity,
+    quote_id: cartId,
+    sku: variant.attributes.sku,
+    ...productOption
+  }
+}
+
+// FIXME: will be replaced with dedicated APIv2 endpoint
+const variantFromSku = (spreeClient: Instance, sku: string): Promise<JsonApiSingleResponse> => {
+  return new Promise((resolve, reject) => {
+    mapPages(
+      (page: number, perPage: number): any => {
+        return spreeClient.products.list({
+          include: 'default_variant,variants',
+          page,
+          per_page: perPage
+        })
+      },
+      (response: JsonApiSingleResponse) => {
+        const product: JsonApiDocument = response.data
+        const relationships = product.relationships
+        const defaultVariantIdentifier = relationships.default_variant.data
+        const defaultVariant = findIncluded(response, defaultVariantIdentifier.type, defaultVariantIdentifier.id)
+        const allVariants = [defaultVariant, ...findIncludedOfType(response, product, 'variants')]
+        const skuV = allVariants.find((v) => {
+          return sku === v.attributes.sku
+        })
+        if (skuV) {
+          resolve({
+            data: skuV,
+            included: response.included
+          })
+        }
+      },
+      100000,
+      100000
+    )
+      .then(() => {
+        reject(new Error('Cannot find sku = ' + sku))
+      })
+      .catch((error) => {
+        console.error(error)
+        reject(error)
+      })
+  })
+}
+
 export {
   findOptionTypeFromOptionValue,
   generateOptionAttributeCode,
-  generateCustomAttributeCode
+  generateCustomAttributeCode,
+  getLineItem,
+  variantFromSku
 }
